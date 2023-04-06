c5d97fd66a4ae1aa85cd6f10c7d556ffca2dc33d
余明明 <709410814@qq.com>
2023-04-06 09:43:00 +0800

    删除了LED灯闪烁

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -12,36 +12,6 @@ int main(void)
 	I2C2_Init();		//硬件IIC初始化
 	SPI1_Init();		//SPI初始化
 	
-	
-	//LED0闪烁线程
-	led0 = rt_thread_init(&led0_thread,
-												"led0",
-												led0_entry,
-												RT_NULL,
-												&led0_stack[0],
-												sizeof(led0_stack),
-												RT_THREAD_PRIORITY_MAX / 2,
-												20
-												);
-	if(led0 == RT_EOK)
-	{
-		rt_thread_startup(&led0_thread);
-	}
-	//LED1闪烁线程
-	led1 = rt_thread_init(&led1_thread,
-												"led1",
-												led1_entry,
-												RT_NULL,
-												&led1_stack[0],
-												sizeof(led1_stack),
-												RT_THREAD_PRIORITY_MAX / 2,
-												20
-												);
-	if(led1 == RT_EOK)
-	{
-		rt_thread_startup(&led1_thread);
-	}
-	
 	return RT_EOK;
 }
 
